ec82326340d3a03bb9c3804612ca46f6daff7a04
Angelay2 <y2zaijian@126.com>
2020-01-26 00:00:20 +0800

    char类型变量设计

---
+++ test8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 设计char类型变量的目的是为了让它们容纳字符型值，但字符在本质上是最小整形值
+// 缺省的char 要么是signed char, 要么是unsigned char,取决于编译器
+int main(){
+	
+	system("pause");
+	return 0;
+}
